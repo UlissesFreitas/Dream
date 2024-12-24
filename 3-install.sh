66dbcc8aec8f19613ca8ab1f9d194d223d368fca
Ulisses <ulisses28filho@gmail.com>
2024-12-24 19:34:24 -0300

    update

--- 3-install.sh
+++ 3-install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 sudo yum install docker -y
+sudo yum install libxcrypt-compat -y
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo mkdir /var/www/html/site-docker
+
+docker build -t enginex .
+
+docker-compose up -d
 
 sudo systemctl start docker
 sudo systemctl enable docker
